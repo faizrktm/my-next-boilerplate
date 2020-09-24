6164fb9ce5bc1871d2209ee48863429c20a67e46
faizrktm <faizkort@gmail.com>
2020-09-24 15:46:50 +0700

    upgrade candi-ui

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,13 @@
 import App from 'next/app';
 import { appWithTranslation } from 'utils/i18n';
-import { MyThemeProvider, GlobalStyles } from 'candi-ui';
+import { CandiProvider, GlobalStyles } from 'candi-ui';
 
 // eslint-disable-next-line react/prop-types
 const MyApp = ({ Component, pageProps }) => (
-  <MyThemeProvider>
+  <CandiProvider>
     <GlobalStyles />
     <Component {...pageProps} />
-  </MyThemeProvider>
+  </CandiProvider>
 );
 
 MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) });
